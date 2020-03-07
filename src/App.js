a484c36589d7b2b0f527db2e83a3bb3864d575a4
Yisel Mesa <yiselm@uninorte.edu.co>
2020-03-07 14:17:56 -0500

    weather

--- src/App.js
+++ src/App.js
@@ -12,10 +12,7 @@ class App extends Component {
     return (
 <div className="principal">
 <nav className="navbar navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">Weather 611 🌤 </a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+  <a className="navbar-brand" href="#">Weather 🌤 </a>
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
     <ul className="navbar-nav">
       <li className="nav-item active">
